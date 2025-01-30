d113fc30076e9c0205ffd0ba52e5c32ac52a3064
carloscl2001 <99760570+carloscl2001@users.noreply.github.com>
2025-01-30 20:57:49 +0100

    Update home_screen.dart

--- esi_planner/lib/screens/home_screen.dart
+++ esi_planner/lib/screens/home_screen.dart
@@ -126,7 +126,7 @@ class HomeScreenState extends State<HomeScreen> {
 Widget build(BuildContext context) {
   return Scaffold(
     appBar: AppBar(
-      title: const Text('Asignaturas disponibles', style: TextStyle(color: Colors.white)),
+      title: const Text('Tus clases', style: TextStyle(color: Colors.white)),
       centerTitle: true,
     ),
     body: isLoading
@@ -144,119 +144,124 @@ Widget build(BuildContext context) {
                   const SizedBox(height: 20),
                 ],
                 Expanded(
-                  child: ListView.builder(
-                    itemCount: subjects.length,
-                    itemBuilder: (context, index) {
-                      final subject = subjects[index];
-
-                      // Agrupar los eventos por fecha
-                      Map<String, List<Map<String, dynamic>>> groupedByDate = {};
-
-                      for (var classData in subject['classes']) {
-                        for (var event in classData['events']) {
-                          final eventDate = event['date'];
-
-                          if (!groupedByDate.containsKey(eventDate)) {
-                            groupedByDate[eventDate] = [];
-                          }
-
-                          // Agregar el evento a la fecha correspondiente
-                          groupedByDate[eventDate]!.add({
-                            'classType': classData['type'], // Guardar el tipo de clase directamente
-                            'event': event, // Guardar el evento completo
-                          });
-                        }
-                      }
-
-                      // Ordenar los eventos por hora dentro de cada fecha
-                      groupedByDate.forEach((date, events) {
-                        events.sort((a, b) {
-                          DateTime timeA = DateTime.parse('${a['event']['date']} ${a['event']['start_hour']}');
-                          DateTime timeB = DateTime.parse('${b['event']['date']} ${b['event']['start_hour']}');
-                          return timeA.compareTo(timeB);
-                        });
-                      });
-
-                      return Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          // Mostrar las clases agrupadas por fecha
-                          ...groupedByDate.entries.map<Widget>((entry) {
-                            final date = entry.key;
-                            final events = entry.value;
-
-                            return Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: <Widget>[
-                                // Título con la fecha
-                                Padding(
-                                  padding: const EdgeInsets.symmetric(vertical: 8.0),
-                                  child: Text(
-                                    'Fecha: $date',
-                                    style: const TextStyle(
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.indigo,
-                                    ),
-                                  ),
-                                ),
-                                // Cards para cada evento
-                                ...events.map<Widget>((eventData) {
-                                  final event = eventData['event'];
-                                  final classType = eventData['classType'];
-
-                                  return Card(
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(12.0),
-                                    ),
-                                    elevation: 4,
-                                    margin: const EdgeInsets.symmetric(vertical: 8.0),
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(16.0),
-                                      child: Column(
-                                        crossAxisAlignment: CrossAxisAlignment.start,
-                                        children: [
-                                          // Nombre de la asignatura dentro de la tarjeta
-                                          Text(
-                                            subject['name'] ?? 'No Name',
-                                            style: const TextStyle(
-                                              fontWeight: FontWeight.bold,
-                                              fontSize: 16,
-                                              color: Colors.indigo,
-                                            ),
-                                          ),
-                                          const SizedBox(height: 8),
-                                          // Tipo de clase
-                                          Text(
-                                            'Tipo de clase: ${classType ?? 'No disponible'}',
-                                            style: const TextStyle(
-                                              fontWeight: FontWeight.w500,
-                                            ),
-                                          ),
-                                          const SizedBox(height: 8),
-                                          // Información del evento
-                                          Text(
-                                            'Hora: ${event['start_hour']} - ${event['end_hour']}',
-                                          ),
-                                          Text(
-                                            'Ubicación: ${event['location']}',
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                  );
-                                }).toList(),
-                              ],
-                            );
-                          }).toList(),
-                        ],
-                      );
-                    },
-                  ),
+                  child: _buildEventList(),
                 ),
               ],
             ),
           ),
+  );
+}
+
+Widget _buildEventList() {
+  // Recopilar todos los eventos de todas las asignaturas
+  List<Map<String, dynamic>> allEvents = [];
+  for (var subject in subjects) {
+    for (var classData in subject['classes']) {
+      for (var event in classData['events']) {
+        allEvents.add({
+          'subjectName': subject['name'] ?? 'No Name',
+          'classType': classData['type'] ?? 'No disponible',
+          'event': event,
+        });
+      }
+    }
+  }
+
+  // Agrupar los eventos por fecha
+  Map<String, List<Map<String, dynamic>>> groupedByDate = {};
+  for (var eventData in allEvents) {
+    final eventDate = eventData['event']['date'];
+
+    if (!groupedByDate.containsKey(eventDate)) {
+      groupedByDate[eventDate] = [];
+    }
+
+    groupedByDate[eventDate]!.add(eventData);
+  }
+
+  // Ordenar los eventos por hora dentro de cada fecha
+  groupedByDate.forEach((date, events) {
+    events.sort((a, b) {
+      DateTime timeA = DateTime.parse('${a['event']['date']} ${a['event']['start_hour']}');
+      DateTime timeB = DateTime.parse('${b['event']['date']} ${b['event']['start_hour']}');
+      return timeA.compareTo(timeB);
+    });
+  });
+
+  // Ordenar las fechas
+  var sortedDates = groupedByDate.keys.toList()..sort();
+
+  return ListView.builder(
+    itemCount: sortedDates.length,
+    itemBuilder: (context, index) {
+      final date = sortedDates[index];
+      final events = groupedByDate[date]!;
+
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          // Título con la fecha
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: Text(
+              'Fecha: $date',
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Colors.indigo,
+              ),
+            ),
+          ),
+          // Cards para cada evento
+          ...events.map<Widget>((eventData) {
+            final event = eventData['event'];
+            final classType = eventData['classType'];
+            final subjectName = eventData['subjectName'];
+
+            return Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12.0),
+              ),
+              elevation: 4,
+              margin: const EdgeInsets.symmetric(vertical: 8.0),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // Nombre de la asignatura dentro de la tarjeta
+                    Text(
+                      subjectName,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                        color: Colors.indigo,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    // Tipo de clase
+                    Text(
+                      'Tipo de clase: $classType',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    // Información del evento
+                    Text(
+                      'Hora: ${event['start_hour']} - ${event['end_hour']}',
+                    ),
+                    Text(
+                      'Ubicación: ${event['location']}',
+                    ),
+                  ],
+                ),
+              ),
+            );
+          }).toList(),
+        ],
+      );
+    },
   );
 }
 
