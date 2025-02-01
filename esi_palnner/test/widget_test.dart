b015e68a55ffdcba0185882082804034bc3ea5ac
carloscl2001 <99760570+carloscl2001@users.noreply.github.com>
2025-02-01 16:09:39 +0100

    Update widget_test.dart

--- esi_palnner/test/widget_test.dart
+++ esi_palnner/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:esi_palnner/main.dart';
+import 'package:esi_planner/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
